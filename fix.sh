8932219f504ad338aa6a86db6365824e50c6bddd
jj <8@qq.com>
2026-07-04 02:36:48 +0800

    f

--- fix.sh
+++ fix.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-username=`git config user.name`
-if [ -z $username ];then
-	username=`whoami`
-fi
-comment=$1
-if [ -z "$comment" ];then
-	comment="$username commit at `date +'%Y-%m-%d %H:%M:%S'`"
-fi
-git add .;git commit -m "$comment" ;git push
 
-if [ -f ~/fix.sh ];then
-	cp ~/fix.sh ./fix.sh
-else 
-	cp ./fix.sh ~/fix.sh
-fi
+git add .;git commit -m "$comment" ;
+# git push
